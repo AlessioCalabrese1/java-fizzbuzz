f9a680e58cb008155073abe4b89ed30c6e54cb93
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-11-15 17:29:21 +0100

    FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i == 0) {
+                System.out.print(i);
+            }else if ((i % 15) == 0) {
+                System.out.print(" - " + "FizzBuz");
+            }else if((i % 3) == 0){
+                System.out.print(" - " + "Fizz");
+            }else if((i % 5) == 0){
+                System.out.print(" - " + "Buzz");
+            }else{
+                System.out.print(" - " + i);
+            }
+        }
+    }
+}
